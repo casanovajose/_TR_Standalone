8a95b45251410af9a00328b7678b0793261a2fc7
jose casanova <jose.casanova@nayra.coop>
2021-07-18 01:05:04 -0300

    adding style basics and draggable windows

--- front/js/_TR.js
+++ front/js/_TR.js
@@ -1,4 +1,50 @@
+// DRAG & MOVE WINDOWS
 
+// Make the DIV element draggable:
+dragElement(document.getElementById("window1"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "-header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// UTILS
 
 function renderSVGs(id, xml_string){
   var doc = new DOMParser().parseFromString(`<svg class="paths" xmlns="http://www.w3.org/2000/svg" width="400" height="400">${xml_string}</svg>`, 'application/xml');
@@ -62,7 +108,7 @@ function drawPathPoints(canvas, points) {
   });
 }
 
-
+// THE CANVASes CODE
 (function() {
   const $ = function(id){return document.getElementById(id)};
   let currentPoints = [];
